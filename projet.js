d194c1adcfec5aa18a6f612ff67395a0b2d44f77
yassine <yassinemouhcine934@gmail.com>
2026-09-09 09:47:11 +0100

    1. Menu principal

--- projet.js
+++ projet.js
@@ -1,1 +1,52 @@
 
+const prompt = require('prompt-sync')()
+let choix;
+while(choix!==0){
+console.log(`=================================
+
+        RAILWAY MANAGER
+
+=================================
+
+1.Afficher les trajet
+2. Acheter un ticket
+3. Afficher les tickets
+4. Annuler un ticket
+5. Rechercher un ticket
+6. Filtrer les trajets
+7. Trier les trajets
+0. Quitter`)
+choix= Number(prompt("entrer votre choix :"))
+switch(choix){
+    case 1:
+        console.log("Afficher les trajet")
+        break
+        case 2:
+          console.log("Acheter un ticket")
+        break
+         case 3:
+console.log("Afficher les tickets")
+        break
+          case 4:
+console.log("Annuler un ticket")
+        break
+            case 5:
+console.log("Rechercher un ticket")
+        break
+        case 6:
+console.log("Filtrer les trajets")
+        break
+        case 7:
+console.log("Trier les trajets")
+        break
+        case 0:
+console.log("Quiter")
+        break
+        default :
+        console.log("entrer un nombre de 0 a 7")
+        break
+}
+
+
+}
+
